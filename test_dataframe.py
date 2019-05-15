b1a41e3c8fe4c944727b734945cc365833fca0d8
Stremmel <jstremme@LAMU02T9E48GTFM.uhc.com>
2019-05-15 07:28:49 -0700

    add more descriptive test names and fix double negative test case

--- test_dataframe.py
+++ test_dataframe.py
@@ -6,13 +6,13 @@ from dataframe import read_congress_terms
 class UnitTests(unittest.TestCase):
     '''This class contains the test functions to execute'''
 
-    def test_rows(self):
+    def test_rows_greater_than_one(self):
         '''Test that dataframe has at least one row'''
 
         data = read_congress_terms()
-        self.assertFalse(data.shape[0] < 1)
+        self.assertTrue(data.shape[0] > 1)
 
-    def test_cols(self):
+    def test_expected_cols_present(self):
         '''Test that dataframe has the expected columns'''
 
         data = read_congress_terms()
